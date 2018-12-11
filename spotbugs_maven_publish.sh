36ca0adb873bd4bf3b235a0bb48eb8591931cf1a
Vincent Latombe <vincent@latombe.net>
2018-12-11 09:33:27 +0100

    Remove provider, it is no longer accepted by 'jx step collect'. Default to GH pages

--- spotbugs_maven_publish.sh
+++ spotbugs_maven_publish.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jx step collect --provider=${JX_SPOTBUGS_MAVEN_PUBLISH_PROVIDER} --pattern=target/spotbugsXml.xml --classifier=spotbugs
+jx step collect --pattern=target/spotbugsXml.xml --classifier=spotbugs
